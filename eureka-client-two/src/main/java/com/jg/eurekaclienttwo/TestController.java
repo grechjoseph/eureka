ab6255c1b621c2ba891497f06d242ee612ec5204
josephgrech <josephgrech90@gmail.com>
2020-03-10 23:46:21 +0100

    Added Hystrix to Client Two for when call to Client One fails.

--- eureka-client-two/src/main/java/com/jg/eurekaclienttwo/TestController.java
+++ eureka-client-two/src/main/java/com/jg/eurekaclienttwo/TestController.java
@@ -1,17 +1,26 @@
 package com.jg.eurekaclienttwo;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@EnableCircuitBreaker
 public class TestController {
 
     @Autowired
     private EurekaClientOneClient clientOneClient;
 
+    @HystrixCommand(fallbackMethod = "defaultForGetString")
     @GetMapping("/test")
     public String getString() {
         return "Hello " + clientOneClient.getString() + "!";
+    }
+
+    public String defaultForGetString() {
+        System.out.println("Failed to run getString.");
+        return "Default";
     }
 }
